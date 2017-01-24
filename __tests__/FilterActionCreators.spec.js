db5f418a39577c9e5e7616d29e35217d4d519a53
Alexander Duka <a.duka@aimprosoft.com>
2017-01-24 11:49:57 +0200

    filter tests

---
+++ __tests__/FilterActionCreators.spec.js
@@ -0,0 +1,32 @@
+import { FilterStore } from '../src/assets/js/stores';
+import { FilterActions } from '../src/assets/js/actions';
+
+describe('FilterActions', function () {
+
+  it('can toggle show done', function () {
+
+    let state = FilterStore.getState();
+
+    const { showDone } = state;
+
+    FilterActions.toggleIsDone();
+
+    state = FilterStore.getState();
+
+    expect(state.showDone).toBe(!showDone);
+
+  });
+
+  it('can change filter', function () {
+
+    const nextFilter = 'next_filter';
+
+    FilterActions.changeFilter(nextFilter);
+
+    const state = FilterStore.getState();
+
+    expect(state.filter).toBe(nextFilter);
+
+  });
+
+});
